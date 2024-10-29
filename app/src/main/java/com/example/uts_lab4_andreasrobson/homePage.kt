a6b38d27990754aa0a92514299dc4651eb1fb5a0
Andreas Robson Simanjuntak <xandersimanjuntak@gmail.com>
2024-10-29 17:11:40 +0700

    intent done

--- app/src/main/java/com/example/uts_lab4_andreasrobson/homePage.kt
+++ app/src/main/java/com/example/uts_lab4_andreasrobson/homePage.kt
@@ -1,16 +1,25 @@
 package com.example.uts_lab4_andreasrobson
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.cardview.widget.CardView
 
 class homePage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_home_page)
+
+        val godestinationcard = findViewById<CardView>(R.id.destinationCard)
+        godestinationcard.setOnClickListener {
+            val intent = Intent(this, kelimutuPage::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/com/example/uts_lab4_andreasrobson/kelimutuPage.kt
+++ app/src/main/java/com/example/uts_lab4_andreasrobson/kelimutuPage.kt
@@ -1,8 +1,12 @@
 package com.example.uts_lab4_andreasrobson
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import android.net.Uri
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -11,10 +15,24 @@ class kelimutuPage : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_kelimutu_page)
+
+        val ivBack = findViewById<ImageView>(R.id.ivBack)
+        ivBack.setOnClickListener {
+            val intent = Intent(this, homePage::class.java)
+            startActivity(intent)
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val btnBookNow = findViewById<Button>(R.id.btnBookNow)
+        btnBookNow.setOnClickListener {
+            val url = "https://sailingkomodo.com/kelimutu-sunrise-tour/"
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+            startActivity(intent)
         }
     }
 }
